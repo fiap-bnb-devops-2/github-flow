ada8e3e0567e61c3ab853b636d0288bf86e41296
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-09-03 11:31:38 -0300

    feat: changing text in app.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aprendendo agora o GitHub Flow
         </p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
