e11fb98cf651075bf57a80a139ce3843901ba3cf
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-09-05 10:02:50 -0300

    feat: login tag

--- src/Login.js
+++ src/Login.js
@@ -4,6 +4,7 @@ function Login() {
     return (
         <div className="App">
             <header className="App-header">
+                <p>Arquivo de Login</p>
             </header>
         </div>
     );
